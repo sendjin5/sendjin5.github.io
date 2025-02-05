5ca2f2560d956c60e4395362791d09052bf8e964
sendjin5 <sendjin5@gmail.com>
2025-02-05 13:19:23 +0900

    react migration

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,56 +1,26 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./js/basic/Header";
+import Home from "./js/main/Home";
+import Portfolio from "./js/main/Portfolio";
+import Animation from "./js/main/Animation";
+import "./css/basic.css";
+
+const App = () => {
+  const [selectedRoute, setSelectedRoute] = useState("/");
+
   return (
-    <div class="container">
-      <div class="mainBox">
-        <div class="mainIn">
-          <div class="loader"></div>
-        </div>
-        <div class="mainIn">
-          <div class="loader2">
-            <div class="box box-1">
-              <div class="side-left"></div>
-              <div class="side-right"></div>
-              <div class="side-top"></div>
-            </div>
-            <div class="box box-2">
-              <div class="side-left"></div>
-              <div class="side-right"></div>
-              <div class="side-top"></div>
-            </div>
-            <div class="box box-3">
-              <div class="side-left"></div>
-              <div class="side-right"></div>
-              <div class="side-top"></div>
-            </div>
-            <div class="box box-4">
-              <div class="side-left"></div>
-              <div class="side-right"></div>
-              <div class="side-top"></div>
-            </div>
-          </div>
+    <Router>
+      <div className="flex_between">
+        <Header setSelectedRoute={setSelectedRoute} />
+        <div className="container">
+          {selectedRoute === "/" && <Home />}
+          {selectedRoute === "/Portfolio" && <Portfolio />}
+          {selectedRoute === "/Animation" && <Animation />}
         </div>
       </div>
-      <div class="mainBox">
-        <div class="mainIn">
-          <div class="loadingspinner">
-            <div id="square1"></div>
-            <div id="square2"></div>
-            <div id="square3"></div>
-            <div id="square4"></div>
-            <div id="square5"></div>
-          </div>
-        </div>
-        <div class="mainIn">
-          <div class="newtons-cradle">
-            <div class="newtons-cradle__dot"></div>
-            <div class="newtons-cradle__dot"></div>
-            <div class="newtons-cradle__dot"></div>
-            <div class="newtons-cradle__dot"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
