8263f12ca91341a004f8f26454372943fc1869e9
sendjin5 <sendjin5@gmail.com>
2025-02-11 08:00:21 +0900

    3.js update

--- react-app/src/js/main/ThreeCom/LandTop.js
+++ react-app/src/js/main/ThreeCom/LandTop.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 
-export function Box() {
+export function LandTop() {
   const meshRef = useRef(); // Three.js의 mesh를 직접 조작하기 위한 ref 생성
 
   // 애니메이션 효과 추가 (매 프레임마다 실행됨)
@@ -11,11 +11,11 @@ export function Box() {
   });
 
   return (
-    <mesh ref={meshRef} scale={1}>
+    <mesh ref={meshRef} scale={0.8}>
       {/* 박스(메쉬) 생성, ref를 연결하여 조작 가능하도록 함 */}
-      <boxGeometry args={[2, 0.5, 1]} />
+      <boxGeometry args={[1, 1, 1]} />
       {/* 박스의 크기 (가로, 세로, 높이) */}
-      <meshStandardMaterial color="#f7e479" />
+      <meshStandardMaterial color="blue" />
       {/* 박스의 색상을 설정 */}
     </mesh>
   );
--- react-app/src/js/main/ThreeDimension.js
+++ react-app/src/js/main/ThreeDimension.js
@@ -3,15 +3,17 @@ import { Canvas } from "@react-three/fiber"; // Three.js를 React에서 쉽게 �
 import { OrbitControls } from "@react-three/drei"; // 마우스로 뷰를 조작할 수 있도록 해주는 유틸
 import { Box } from "./ThreeCom/Box"; // 별도의 컴포넌트로 분리한 박스
 import { Land } from "./ThreeCom/Land"; // 별도의 컴포넌트로 분리한 박스
+import { LandTop } from "./ThreeCom/LandTop"; // 별도의 컴포넌트로 분리한 박스
 
 const ThreeDimension = () => {
   return (
-    <Canvas camera={{ position: [0, 1, 5] }}>
+    <Canvas camera={{ position: [5, 1, 4] }}>
       {/* 3D 공간을 렌더링하는 기본 캔버스, 카메라 초기 위치 설정 */}
       <ambientLight intensity={1} />
       {/* 전체적인 환경광 (빛) 추가 */}
       <directionalLight position={[5, 0, 5]} intensity={3} />
       {/* 특정 방향에서 비추는 빛 추가 */}
+      <LandTop />
       <Box />
       <Land />
       {/* 박스 컴포넌트 추가 */}
